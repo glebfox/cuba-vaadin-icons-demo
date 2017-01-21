dda17d6fd2bdf12f3615f4fb79d8e91c17d0c827
Gleb Gorelov <glebfox@gmail.com>
2017-01-21 21:33:48 +0400

    Add svg icons for constraint edit window

---
+++ modules/web/src/com/glebfox/icons/web/app/security/constraint/edit/ExtConstraintEditor.java
@@ -0,0 +1,6 @@
+package com.glebfox.icons.web.app.security.constraint.edit;
+
+import com.haulmont.cuba.gui.app.security.constraint.edit.ConstraintEditor;
+
+public class ExtConstraintEditor extends ConstraintEditor {
+}
